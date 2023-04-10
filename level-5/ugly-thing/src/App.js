1aa0219090bd1f07cb77db6733d9caa02ef3fab9
Natalie Taylor <sisterchicago@gmail.com>
2023-04-10 11:26:45 -0600

    added level-5/ adjustments

---
+++ level-5/ugly-thing/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Form from './Form'
+import UglyList from './UglyDisplay'
+import Header from './Header'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Form />
+      <UglyList />
+    </div>
+  )
+}
+
+export default App
---
+++ level-5/ugly-thing/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="heading">
+            Bring On the Uglies
+        </div>
+    )
+}
+
+export default Header 
